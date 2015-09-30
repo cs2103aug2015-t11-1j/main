79a8332a12383f03f746124ab530f31fa621d171
Idawati Bustan <idawatibustan@users.noreply.github.com>
2015-10-01 02:18:21 +0800

    Modify Storage.java, add attributes, constructor

--- src/planit/Storage.java
+++ src/planit/Storage.java
@@ -1,8 +1,35 @@
 package planit;
 
-public class Storage {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Storage {
+	private String mainFileName;
+	private File mainFile;
+	private String toDoFileName;
+	private File toDoFile;
+	
+	public Storage(){
+		mainFile = new File("main.txt");
+		this.checkFileExist(mainFile);
+		mainFileName = "main.txt";
+		toDoFile = new File("todo.txt");
+		this.checkFileExist(toDoFile);
+		mainFileName = "todo.txt";
+	}
+	
+	FileWriter fwMain = new FileWriter(this.mainFile, true);
+	FileWriter fwTodo= new FileWriter(this.toDoFile, true);
+	
+	//
 	public static String storeNewEvent(Command userCommand) {
+//		String item = userCommand.
+		if(userCommand.getUserTimeStart().equals("null")){
+			
+		} else {
+			
+		}
 		return null;
 		// TODO Auto-generated method stub
 		// Return String with <event><dateString><timeString>
@@ -18,5 +45,15 @@ public class Storage {
 		
 	}
 
+	//catch IOException create file
+	private void checkFileExist(File file) {
+		try{
+			if(!file.exists()){
+				file.createNewFile();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
