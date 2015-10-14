9b5a3cbd7e4533be0688237022b0495973d3b6bd
Ruffomao <jtjudo93@gmail.com>
2015-10-15 00:03:37 +0800

    Recommit changes which was overwritten

--- src/planit/StringParserTest.java
+++ src/planit/StringParserTest.java
@@ -2,7 +2,11 @@ package planit;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import planit.StringParser.ACTION_TYPE;
 
 import org.junit.Before;
@@ -13,7 +17,7 @@ import com.sun.media.sound.InvalidFormatException;
 public class StringParserTest {
 	private StringParser sp;
 	private Command cmd;
-	private Command expectedCmd;
+	private String todayDate;
 	ArrayList<String> dateArray;
 	ArrayList<String> timeArray;
 	
@@ -21,7 +25,10 @@ public class StringParserTest {
 	public void setUp() {
 		sp = new StringParser();
 		cmd = new Command();
-		expectedCmd = new Command();
+		//expectedCmd = new Command();
+		Date today = Calendar.getInstance().getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
+		todayDate = sdf.format(today);
 	}
 	
 	@Test
@@ -29,25 +36,78 @@ public class StringParserTest {
 		String test = "add event > 123456 1234 to 654321 4321";
 		cmd = sp.parseStringIntoCommand(test);
 		
-		/*expectedCmd.setUserStringInput(test);
-		expectedCmd.setUserCommand("add");
-		expectedCmd.setUserActionType(ACTION_TYPE.ADD);
-		expectedCmd.setUserEventTask("event");*/
-		
 		dateArray = new ArrayList<String>(2);
 		timeArray = new ArrayList<String>(2);
 		
 		dateArray.add("123456");
 		dateArray.add("654321");
-		expectedCmd.setUserDate(dateArray);
 		timeArray.add("1234");
 		timeArray.add("4321");
-		expectedCmd.setUserTime(timeArray);
 		
 		assertEquals("add", cmd.getUserCommand());
 		assertEquals("event", cmd.getUserEventTask());
+		assertNull(cmd.getUserUpdateEventTask());
 		assertEquals(dateArray, cmd.getUserDateRange());
 		assertEquals(timeArray, cmd.getUserTimeRange());
 		assertEquals(ACTION_TYPE.ADD, cmd.getActionType());
 	}
+	
+	@Test
+	public void testCommandObject2() throws InvalidFormatException {
+		String test = "add event > 1234 to 4321";
+		cmd = sp.parseStringIntoCommand(test);
+		
+		dateArray = new ArrayList<String>(2);
+		timeArray = new ArrayList<String>(2);
+		
+		dateArray.add(todayDate);
+		dateArray.add("??????");
+		timeArray.add("1234");
+		timeArray.add("4321");
+		
+		assertEquals("add", cmd.getUserCommand());
+		assertEquals("event", cmd.getUserEventTask());
+		assertNull(cmd.getUserUpdateEventTask());
+		assertEquals(dateArray, cmd.getUserDateRange());
+		assertEquals(timeArray, cmd.getUserTimeRange());
+		assertEquals(ACTION_TYPE.ADD, cmd.getActionType());
+	}
+	
+	@Test
+	public void testCommandObject3() throws InvalidFormatException {
+		String test = "add floating event";
+		cmd = sp.parseStringIntoCommand(test);
+		
+		dateArray = new ArrayList<String>(2);
+		timeArray = new ArrayList<String>(2);
+		
+		dateArray.add("??????");
+		dateArray.add("??????");
+		
+		assertEquals("add", cmd.getUserCommand());
+		assertEquals("floating event", cmd.getUserEventTask());
+		assertNull(cmd.getUserUpdateEventTask());
+		assertEquals(dateArray, cmd.getUserDateRange());
+		assertTrue("timeArray is not empty", cmd.getUserTimeRange().isEmpty());
+		assertEquals(ACTION_TYPE.ADD, cmd.getActionType());
+	}
+	
+	@Test
+	public void testCommandObject4() throws InvalidFormatException {
+		String test = "update this event : that event";
+		cmd = sp.parseStringIntoCommand(test);
+		
+		assertEquals("update", cmd.getUserCommand());
+		assertEquals("this event", cmd.getUserEventTask());
+		assertEquals("that event", cmd.getUserUpdateEventTask());
+		assertTrue("dateArray is not empty", cmd.getUserDateRange().isEmpty());
+		assertTrue("timeArray is not empty", cmd.getUserTimeRange().isEmpty());
+		assertEquals(ACTION_TYPE.UPDATE, cmd.getActionType());
+	}
+	
+	/*@Test (expected = InvalidFormatException.class)
+	public void testCommandObject4() throws InvalidFormatException {
+		String test = "done this event > 1234";
+		cmd = sp.parseStringIntoCommand(test);
+	}*/
 }
