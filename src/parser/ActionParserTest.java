9ff9236ff013b32f2ac23cd239e640ac691e18b4
Ruffomao <jtjudo93@gmail.com>
2015-10-21 16:42:05 +0800

    Updated ActionParserTest to cover all possible paths

--- src/parser/ActionParserTest.java
+++ src/parser/ActionParserTest.java
@@ -16,22 +16,29 @@ public class ActionParserTest {
 	@Before
 	public void setUp() {
 		arr.add(ACTION_TYPE.ADD);
-		arr.add(ACTION_TYPE.DELETE);
-		arr.add(ACTION_TYPE.SHOW);
-		arr.add(ACTION_TYPE.INVALID);
 		arr.add(ACTION_TYPE.ADD);
 		arr.add(ACTION_TYPE.DELETE);
+		arr.add(ACTION_TYPE.DELETE);
+		arr.add(ACTION_TYPE.DELETE);
+		arr.add(ACTION_TYPE.SHOW);
 		arr.add(ACTION_TYPE.SHOW);
 		arr.add(ACTION_TYPE.SEARCH);
+		arr.add(ACTION_TYPE.SEARCH);
+		arr.add(ACTION_TYPE.UPDATE);
+		arr.add(ACTION_TYPE.UPDATE);
+		arr.add(ACTION_TYPE.UPDATE);
 		arr.add(ACTION_TYPE.DONE);
 		arr.add(ACTION_TYPE.DONE);
 		arr.add(ACTION_TYPE.UNDO);
 		arr.add(ACTION_TYPE.EXIT);
+		arr.add(ACTION_TYPE.EXIT);
+		arr.add(ACTION_TYPE.INVALID);
 	}
 
 	@Test
 	public void testActionParser() {
-		String test[] = { "add", "del", "show", "LOL", "new", "rm", "display", "find", "done", "mark", "undo", "QUIT" };
+		String test[] = { "ADD", "new", "DELETE", "rm", "dEl", "Show", "disPLAY", "search", "fInd", "edit", "CHANGE",
+				"update", "done", "MarK", "undo", "exit", "quit", "LOL" };
 		for (int i = 0; i < test.length; i++) {
 			ACTION_TYPE result = ActionParser.setUserAction(test[i]);
 			ACTION_TYPE expected = arr.get(i);
