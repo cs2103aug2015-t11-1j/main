8381dbebfd42322f5d407f6c71732e44b4229fa5
Ruffomao <jtjudo93@gmail.com>
2015-10-21 19:08:24 +0800

    Implemented exception handling for EventTaskParser
    1) EventTaskParser now throws relevant exceptions
    2) EventTaskParserTest updated to include all possible paths
    3) Parser updated with try/catch blocks for scenarios where
    setEventTask() is called
    4) Created InvalidInputException for invalid inputs by the user

--- src/parser/EventTaskParserTest.java
+++ src/parser/EventTaskParserTest.java
@@ -5,64 +5,63 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EventTaskParserTest {
-	
+
 	@Test
-	public void testEventTaskParser1() {
+	public void testEventTaskParser1() throws InvalidInputException {
 		String test = "add this event from today till tomorrow";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "this event";
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
-	public void testEventTaskParser2() {
+	public void testEventTaskParser2() throws InvalidInputException {
 		String test = "add this event TODAY";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "this event";
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
-	public void testEventTaskParser3() {
+	public void testEventTaskParser3() throws InvalidInputException {
 		String test = "add this event by MONDAY";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "this event";
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
-	public void testEventTaskParser4() {
+	public void testEventTaskParser4() throws InvalidInputException {
 		String test = "add this event > 10 may";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "this event";
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
-	public void testEventTaskParser5() {
+	public void testEventTaskParser5() throws InvalidInputException {
 		String test = "update 1 meeting";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "meeting";
 		assertEquals(expected, result);
 	}
-	
+
 	@Test
-	public void testEventTaskParser6() {
+	public void testEventTaskParser6() throws InvalidInputException {
 		String test = "add event";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "event";
 		assertEquals(expected, result);
 	}
-	
-	@Test
-	public void testEventTaskParser7() {
-		String test = "del 10";
-		String result = EventTaskParser.getEventTask(test);
-		assertNull(result);
+
+	@Test(expected = InvalidInputException.class)
+	public void testEventTaskParser7() throws InvalidInputException {
+		String test = "add";
+		EventTaskParser.getEventTask(test);
 	}
-	
+
 	@Test
-	public void testEventTaskParser8() {
+	public void testEventTaskParser8() throws InvalidInputException {
 		String test = "add meeting @ NUS tomorrow";
 		String result = EventTaskParser.getEventTask(test);
 		String expected = "meeting @ NUS";
