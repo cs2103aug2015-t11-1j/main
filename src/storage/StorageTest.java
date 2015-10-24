6decba41644dd220e8de467f90e213d8eab66be5
ishsingh93 <ishsingh93@gmail.com>
2015-10-25 01:32:47 +0800

    Done up functionalities for Add, Show and Search

--- src/storage/StorageTest.java
+++ src/storage/StorageTest.java
@@ -21,19 +21,19 @@ public class StorageTest {
 		tl.add(new Task("12/12/15", "1200", "Meeting with Mrs. Awesome"));
 		tl.add(new Task("Buy dog food"));
 		tl.add(new Task("13/12/15", "Holiday today"));
-		assertEquals("? 12/12/15 1200 Meeting with Mrs. Awesome", tl.get(0).toString());
+		assertEquals("0 ? 12/12/15 1200 Meeting with Mrs. Awesome", tl.get(0).toString());
 		State s1 = new State(tl);
 		s1.add(s1.getTaskList().remove(0).markDone());
 		s1.sort();
-		assertEquals("@ 12/12/15 1200 Meeting with Mrs. Awesome", s1.getTaskList().get(1).toString());
+		assertEquals("2 @ 12/12/15 1200 Meeting with Mrs. Awesome", s1.getTaskList().get(1).toString());
 		s.update(s1);
 		State s2 = s.extractState();
-		assertEquals("@ 12/12/15 1200 Meeting with Mrs. Awesome", s2.getTaskList().get(1).toString());
-		assertEquals("? Buy dog food", s2.getTaskList().get(0).toString());
-		assertEquals("? 13/12/15 Holiday today", s2.getTaskList().get(2).toString());
+		assertEquals("2 @ 12/12/15 1200 Meeting with Mrs. Awesome", s2.getTaskList().get(1).toString());
+		assertEquals("1 ? Buy dog food", s2.getTaskList().get(0).toString());
+		assertEquals("3 ? 13/12/15 Holiday today", s2.getTaskList().get(2).toString());
 		
 		s2.add(s2.getTaskList().remove(0).markDone());
 		s2.sort();
-		assertEquals("@ Buy dog food", s2.getTaskList().get(0).toString());
+		assertEquals("1 @ Buy dog food", s2.getTaskList().get(0).toString());
 	}
 }
