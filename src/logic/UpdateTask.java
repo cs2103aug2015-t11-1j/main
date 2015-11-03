f02a5b677b9a75e1692d3c5381af2e94c73e2ed4
ishsingh93 <ishsingh93@gmail.com>
2015-11-03 22:46:09 +0800

    Edited setCurrState() for UpdateTask class

--- src/logic/UpdateTask.java
+++ src/logic/UpdateTask.java
@@ -78,7 +78,7 @@ public class UpdateTask implements Command {
 	
 	@Override
 	public void setCurrState(State state) {
-		currState = state;
+		currState = new State(state);
 	}
 	
 	public String formatDate() {
