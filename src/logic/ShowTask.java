a4f9f60b499aeada10369fc45eb5c715d2580127
ishsingh93 <ishsingh93@gmail.com>
2015-11-03 23:47:05 +0800

    Updated setCurrState() methods and changed the boolean status of HelpTask to 'true'

--- src/logic/ShowTask.java
+++ src/logic/ShowTask.java
@@ -54,7 +54,7 @@ public class ShowTask implements Command {
 	
 	@Override
 	public void setCurrState(State state) {
-		currState = state;
+		currState = new State(state);
 	}
 
 	public boolean getShowDone() {
