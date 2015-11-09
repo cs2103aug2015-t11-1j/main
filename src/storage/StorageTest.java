91d797e33ef95bb2f7a754b1c687ca3e0b48a196
Idawati Bustan <idawatibustan@users.noreply.github.com>
2015-11-09 21:00:05 +0800

    Refactor Task and correct Unit Testing

--- src/storage/StorageTest.java
+++ src/storage/StorageTest.java
@@ -20,26 +20,27 @@ public class StorageTest {
 		assertEquals("Planner.txt", s.getFilePath());
 		
 		ArrayList<Task> tl = new ArrayList<Task>();
-		Handler.clearFile(s._file);
+		Handler.clearFile(Storage._file);
 		tl.add(new Task("12/12/15", "1200", "Meeting with Mrs. Awesome"));
 		tl.add(new Task("Buy dog food"));
 		tl.add(new Task("13/12/15", "Holiday"));
 		assertEquals("0) 12/12/15 1200 Meeting with Mrs. Awesome", tl.get(0).toString());
 		State s1 = new State(tl);
 		
-		
-		s1.add(s1.getTaskList().remove(0).markDone());
 		s1.sort();
-		
-		assertEquals("2 @ 12/12/15 1200 Meeting with Mrs. Awesome", s1.getTaskList().get(1).toString());
+		assertEquals("2) 12/12/15 1200 Meeting with Mrs. Awesome", s1.getTaskList().get(1).toString());
+		//store to storage
 		s.update(s1);
-		State s2 = s.extractState();
-		assertEquals("2 @ 12/12/15 1200 Meeting with Mrs. Awesome", s2.getTaskList().get(1).toString());
-		assertEquals("1 ? Buy dog food", s2.getTaskList().get(0).toString());
-		assertEquals("3 ? 13/12/15 Holiday today", s2.getTaskList().get(2).toString());
 		
+		//extract from file
+		State s2 = s.extractState();
+		assertEquals("2) 12/12/15 1200 Meeting with Mrs. Awesome", s2.getTaskList().get(1).toString());
+		assertEquals("1) Buy dog food", s2.getTaskList().get(0).toString());
+		assertEquals("3) 13/12/15 Holiday", s2.getTaskList().get(2).toString());
+		
+		//sorting lets done task at the bottom
 		s2.add(s2.getTaskList().remove(0).markDone());
 		s2.sort();
-		assertEquals("1 @ Buy dog food", s2.getTaskList().get(0).toString());
+		assertEquals("3) Buy dog food", s2.getTaskList().get(2).toString());
 	}
 }
